8a983709070f48f74e6cf71c20007b2368008868
fauz-del <fauzeeyatagbalaya27@gmail.com>
2026-01-11 04:09:40 +0100

    Update UI layout and components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,11 +6,6 @@ module.exports = {
   ],
   theme: {
     extend: {},
-    colors: {
-      'zen-ivory': '#F9F8F3',
-      'zen-dark': '#121212',
-      'zen-accent': '#C5A358',
-    },
   },
   plugins: [],
 }
